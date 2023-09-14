b0fa27ae1eae737c14f8e90f66fc270847471eda
Nicolas <j.nicolas.suarez37@gmail.com>
2023-09-14 15:25:26 -0300

    creacion product details: Add to cart

---
+++ cypress/e2e/pages/product-details/product-details.methods.js
@@ -0,0 +1,7 @@
+import { productDetailsElements } from "./product-details.elements";
+
+export class productDetailsMethods {
+    static clickOnAddToCartButton() {
+        productDetailsElements.buttons.addToCart.click();
+    }
+}
