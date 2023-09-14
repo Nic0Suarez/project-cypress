b86da6dd94e4e203c1b8b828575501affe536b17
Nicolas <j.nicolas.suarez37@gmail.com>
2023-09-14 01:51:38 -0300

    correcion en generacion de reporte, cuando los tc fallan, los reportes no se generaban.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,14 @@
+import { LoginMethods } from "./pages/login/login.methods"
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    const user = 'qweasdzxc12345'
+    const pass = 'qweasdzxc12345'
+
+    cy.visit('https://www.demoblaze.com/index.html')
+    cy.get('a[data-target="#logInModal"]').click()
+    LoginMethods.login(user, pass)
+    cy.get('a#nameofuser').should('contain.text', user)
+
   })
 })
