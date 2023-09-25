8c08b9da7b24f8aca9472e323389857e8fa0604c
Nicolas <j.nicolas.suarez37@gmail.com>
2023-09-25 16:18:24 -0300

    Caso de Pruebas "Agregar producto al carrito"

--- cypress/e2e/pages/cart/cart.methods.js
+++ cypress/e2e/pages/cart/cart.methods.js
@@ -1,7 +1,11 @@
 import { cartElements } from "./cart.elements";
 
 export class cartMethods {
+
     static clickOnDeleteLink(productName) {
         cartElements.links.delete(productName).click();
+    }
+    static verifyProductAdded(productName) {
+        cartElements.links.delete(productName).should('be.visible');
     }
 }
--- cypress/e2e/pages/product-details/product-details.methods.js
+++ cypress/e2e/pages/product-details/product-details.methods.js
@@ -1,7 +1,15 @@
+import { CommonPageMethods } from "../common-page/common-page.methods";
 import { productDetailsElements } from "./product-details.elements";
 
 export class productDetailsMethods {
+
     static clickOnAddToCartButton() {
         productDetailsElements.buttons.addToCart.click();
+    }
+    static verifyProductDetailsDisplayed() {
+        productDetailsElements.buttons.addToCart.should('be.visible');
+    }
+    static verifyProductAddMessage() {
+        CommonPageMethods.verifyAlert('Product added')
     }
 }
